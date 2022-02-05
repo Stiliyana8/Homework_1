467044db5ebe9a777bc480881b9257a8f561bd31
Stiliyana <stelitu@gmail.com>
2022-02-05 18:23:56 +0200

    Updated Homework1

--- src/main/java/automation/qa/MyFirstHomework.java
+++ src/main/java/automation/qa/MyFirstHomework.java
@@ -9,11 +9,11 @@ public class MyFirstHomework {
 
         Scanner scanner = new Scanner(System.in);
 
-        String name = scanner.next();
+        String name = scanner.nextLine();
 
         System.out.println("Please enter your favourite hobby: ");
 
-        String hobby = scanner.next();
+        String hobby = scanner.nextLine();
 
         System.out.println("Your name is " + name + " and your favourite hobby is " + hobby);
     }
